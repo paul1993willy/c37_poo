0b807b50226f910fb8069c6c84509f8a0202f598
Paul-Willy Jean <paul1993willy@gmail.com>
2013-09-18 09:55:42 -0400

    Améliore les méthodes de ma classe action

--- src/ca/qc/bdeb/c37/semaine3/exercice3/Fraction.java
+++ src/ca/qc/bdeb/c37/semaine3/exercice3/Fraction.java
@@ -4,44 +4,54 @@
  */
 package ca.qc.bdeb.c37.semaine3.exercice3;
 
+import sun.invoke.empty.Empty;
+
 /**
  *
  * @author paulwillyjean
  */
 public class Fraction
 {
-	protected int numerateur, denominateur;
+	protected int numerateur;
+	protected int denominateur;
 
-	public Fraction()
-	{
-		this(1,1);
+	public Fraction() {
+		this(0,1);
 	}
 
-	public Fraction(int numerateur, int denominateur)
-	{
-		this.setNumerateur(numerateur).setDenominateur(denominateur);
+	public Fraction(String fraction) {
+
+		// Aucunement optimisé, mais faire autrement fait chier.
+		this(Integer.parseInt(fraction.split("/", 2)[0]),
+				Integer.parseInt((fraction.split("/", 2).length == 2)
+						? fraction.split("/", 2)[1]
+						: "1"));
 	}
 
-	public int getNumerateur()
-	{
+	public Fraction(int numerateur, int denominateur) {
+		if (denominateur == 0) {
+			throw new IllegalArgumentException("Le denominateur ne doit jamais etre zero");
+		}
+
+		this.numerateur  = numerateur;
+		this.denominateur = denominateur;
+	}
+
+	public int getNumerateur() {
 		return this.numerateur;
 	}
 
-	public Fraction setNumerateur(int numerateur)
-	{
+	public Fraction setNumerateur(int numerateur) {
 		this.numerateur = numerateur;
 		return this;
 	}
 
-	public int getDenominateur()
-	{
+	public int getDenominateur() {
 		return this.denominateur;
 	}
 
-	public Fraction setDenominateur(int denominateur)
-	{
-		if (denominateur == 0)
-		{
+	public Fraction setDenominateur(int denominateur) {
+		if (denominateur == 0) {
 			throw new IllegalArgumentException("Le denominateur ne doit jamais etre zero");
 		}
 
@@ -50,23 +60,16 @@ public class Fraction
 		return this;
 	}
 
-	public Fraction plus(Fraction fractionB)
-	{
+	public Fraction plus(Fraction fractionB) {
 		Fraction fractionResultat = new Fraction();
 
 		// Les dénominateurs doivent être pareils
-		if (this.getDenominateur() != fractionB.getDenominateur())
-		{
-			if ((this.getDenominateur() % fractionB.getDenominateur()) == 0)
-			{
+		if (this.getDenominateur() != fractionB.getDenominateur()) {
+			if ((this.getDenominateur() % fractionB.getDenominateur()) == 0) {
 				fractionResultat.setDenominateur(this.getDenominateur());
-			}
-			else if ((fractionB.getDenominateur() % this.getDenominateur()) == 0)
-			{
+			} else if ((fractionB.getDenominateur() % this.getDenominateur()) == 0) {
 				fractionResultat.setDenominateur(fractionB.getDenominateur());
-			}
-			else
-			{
+			} else {
 				fractionResultat.setDenominateur(this.getDenominateur() * fractionB.getDenominateur());
 			}
 		}
@@ -74,25 +77,25 @@ public class Fraction
 		fractionResultat.setNumerateur((this.getNumerateur() * fractionResultat.getDenominateur() / this.getDenominateur())
 			+ (fractionB.getNumerateur() * fractionResultat.getDenominateur() / fractionB.getDenominateur()));
 
-		return fractionResultat;
+		return fractionResultat.reduire();
 	}
 
 	/**
+	 * Soustrait deux fractions ensemble et en retourne le résultat
 	 * @param {Fraction} fractionB
 	 * @return Fraction
 	 */
-	public Fraction moins(Fraction fractionB)
-	{
+	public Fraction moins(Fraction fractionB) {
 		fractionB.setNumerateur(-fractionB.getNumerateur());
 		return this.plus(fractionB);
 	}
 
 	/**
+	 * Multiplie deux fractions ensemble et en retourne le résultat
 	 * @param {Fraction} fractionB
 	 * @return Fraction
 	 */
-	public Fraction mult(Fraction fractionB)
-	{
+	public Fraction mult(Fraction fractionB) {
 		Fraction fractionResultat = new Fraction();
 
 		fractionResultat.setNumerateur(this.getNumerateur() * fractionB.getNumerateur())
