33f4f660bbc6dbd361ca82ab2fd63fb7aad925ef
Paul-Willy Jean <paul1993willy@gmail.com>
2013-11-20 11:08:17 -0500

    refs #5 Implémente la vue pour le MVC

---
+++ src/ca/qc/bdeb/c37/travailPratique2/views/View.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.qc.bdeb.c37.travailPratique2.views;
+import java.beans.PropertyChangeEvent;
+
+/**
+ *
+ * @author paulwillyjean
+ */
+abstract public class View {
+
+    abstract public void modelPropertyChange(final PropertyChangeEvent evt);
+}
