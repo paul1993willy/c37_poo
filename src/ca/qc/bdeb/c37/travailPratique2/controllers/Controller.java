02ca66495dd9c4a58b49aa7307b3b06448da61ce
Paul-Willy Jean <paul1993willy@gmail.com>
2013-11-20 10:57:26 -0500

    refs #3, refs #2 Implémente le controleur de base du projet

---
+++ src/ca/qc/bdeb/c37/travailPratique2/controllers/Controller.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.qc.bdeb.c37.travailPratique2.controllers;
+import ca.qc.bdeb.c37.travailPratique2.models.Model;
+import ca.qc.bdeb.c37.travailPratique2.views.View;
+import java.util.ArrayList;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.lang.reflect.Method;
+
+/**
+ *
+ * @author paulwillyjean
+ */
+abstract public class Controller implements PropertyChangeListener {
+    private ArrayList<Model> models;
+    private ArrayList<View> views;
+
+    public Controller() {
+        this.models = new ArrayList<Model>();
+        this.views = new ArrayList<View>();
+    }
+
+    public void propertyChange(PropertyChangeEvent evt) {
+        for (View view : this.views) {
+            view.modelPropertyChange(evt);
+        }
+    }
+    public void addModel(Model model) {
+        models.add(model);
+        model.addController(this);
+    }
+
+    public void removeModel(Model model) {
+        models.remove(model);
+        model.removeController(this);
+    }
+
+    public void addView(View view) {
+        views.add(view);
+    }
+
+    public void removeView(View view) {
+        views.remove(view);
+    }
+
+    protected void setModelProperty(String propertyName, Object newValue) {
+        for (Model model : this.models) {
+            try {
+                Method method = model.getClass().getMethod("set" + propertyName,
+                        new Class[] {newValue.getClass()});
+            } catch (NoSuchMethodException nsme) {
+                /*
+                * Does nothing because we don't care
+                */
+            }
+        }
+    }
+}
