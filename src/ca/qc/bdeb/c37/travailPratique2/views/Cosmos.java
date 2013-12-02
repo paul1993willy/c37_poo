7efad7ff94bbac70c8520d35c68f8cf2a1923a61
Paul-Willy Jean <paul1993willy@gmail.com>
2013-12-02 13:37:48 -0500

    refs #Crée une vue Cosmos
    -   Est une instance de JFrame
    -   Implémente l'interface View
    -   Sert à afficher le jeu

---
+++ src/ca/qc/bdeb/c37/travailPratique2/views/Cosmos.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.qc.bdeb.c37.travailPratique2.views;
+import ca.qc.bdeb.c37.travailPratique2.controllers.Controller;
+import ca.qc.bdeb.c37.travailPratique2.controllers.ControllerCosmos;
+
+/**
+ *
+ * @author paulwillyjean
+ */
+public class Cosmos extends javax.swing.JFrame implements View {
+
+    private Controller controller;
+    private javax.swing.JPanel panneau;
+    private javax.swing.JLabel vaisseau;
+
+    public Cosmos(Controller controlleur) {
+        this.controller = controlleur;
+        initComponents();
+        localInitialization();
+    }
+
+    private void initComponents() {
+        panneau = new javax.swing.JPanel(null);
+        vaisseau = new javax.swing.JLabel();
+        this.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 700);
+        this.setLocationRelativeTo(null);
+        this.getContentPane().add(panneau);
+        this.getClass().getResource("background.png");
+        panneau.add(vaisseau);
+        vaisseau.setBounds(225, 550, 50, 50);
+        this.setVisible(true);
+    }
+
+    private void localInitialization() {
+        panneau.addMouseMotionListener(new java.awt.event.MouseMotionListener() {
+
+            @Override
+            public void mouseDragged(java.awt.event.MouseEvent me) {
+            }
+
+            @Override
+            public void mouseMoved(java.awt.event.MouseEvent me) {
+                vaisseau.setLocation(me.getPoint());
+            }
+
+        });
+
+        vaisseau.addComponentListener(new java.awt.event.ComponentListener() {
+
+            @Override
+            public void componentResized(java.awt.event.ComponentEvent ce) {
+            }
+
+            @Override
+            public void componentMoved(java.awt.event.ComponentEvent ce) {
+                ((ControllerCosmos) controller).changetVaisseauPosition(vaisseau.getLocation());
+            }
+
+            @Override
+            public void componentShown(java.awt.event.ComponentEvent ce) {
+            }
+
+            @Override
+            public void componentHidden(java.awt.event.ComponentEvent ce) {
+            }
+        });
+    }
+
+    @Override
+    public void modelPropertyChange(final java.beans.PropertyChangeEvent evt) {
+        if (evt.getPropertyName().equals(ControllerCosmos.VAISSEAU_ICON)) {
+            vaisseau.setIcon( (javax.swing.ImageIcon) evt.getNewValue());
+        }
+    }
+}
