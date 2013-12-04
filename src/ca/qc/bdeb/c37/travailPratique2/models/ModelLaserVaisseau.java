6559d53767de455bfee5235c87346d4da48b8a09
Paul-Willy Jean <paul1993willy@gmail.com>
2013-12-04 18:21:37 -0500

    refs #7 Travaille le modèle de lasers de vaisseau

--- src/ca/qc/bdeb/c37/travailPratique2/models/ModelLaserVaisseau.java
+++ src/ca/qc/bdeb/c37/travailPratique2/models/ModelLaserVaisseau.java
@@ -8,6 +8,7 @@ package ca.qc.bdeb.c37.travailPratique2.models;
 
 import ca.qc.bdeb.c37.travailPratique2.controllers.ControllerCosmos;
 import java.awt.Rectangle;
+import java.net.URL;
 import javax.swing.ImageIcon;
 /**
  *
@@ -15,15 +16,35 @@ import javax.swing.ImageIcon;
  */
 public class ModelLaserVaisseau extends ModelEntite implements ModelLaser {
     static private final int POINTS_GAGNES = 10;
+    private static ImageIcon iconeLaserVaisseau;
+
+    public ModelLaserVaisseau() {
+        init();
+    }
+
+    private void init() {
+        this.setIcon(this.getClass().getResource("Ressources/laserVaisseau"));
+    }
 
     @Override
-    public void deplacer() {
-        this.setY(this.getY() + 1);
-        this.firePropertyChange("Y", this.getY() - 1, this.getY());
+    public synchronized void deplacer() {
+        Rectangle oldBounds = new Rectangle(this.getBounds());
+
+        this.getBounds().setLocation(oldBounds.x, oldBounds.y - 1);
+        this.firePropertyChange(ControllerCosmos.LASER_BOUNDS, oldBounds, this.getBounds());
     }
 
     @Override
     public int detruire() {
         return POINTS_GAGNES;
+    }
+
+    @Override
+    public ImageIcon getIcon() {
+        return iconeLaserVaisseau;
+    }
+
+    public void setIcon(URL ressource) {
+        iconeLaserVaisseau = new ImageIcon(ressource);
     }
 }
